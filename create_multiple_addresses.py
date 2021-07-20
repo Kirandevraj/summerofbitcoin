4751ac2dfda93c66eee5325582868d5e0f951d26
kirandevraj <thekirandevraj@gmail.com>
2021-07-20 22:33:09 +0530

    update

--- create_multiple_addresses.py
+++ create_multiple_addresses.py
@@ -5,6 +5,14 @@ import sys
 import csv
 
 def main(count):
+
+	# create_wallet = subprocess.check_output(["bitcoin-cli","-named","createwallet",'wallet_name=regtest_wallet',"descriptors=true"])
+	# # wallet = subprocess.check_output(["bitcoin-cli", "loadwallet", "regtest_wallet"])
+	# mining_addr = subprocess.check_output(["bitcoin-cli","getnewaddress"])
+	# mining_addr = str(mining_addr.decode("utf-8").replace("\n", ""))
+	# print(mining_addr)
+	# mine_coins = subprocess.check_output(["bitcoin-cli","generatetoaddress","150", mining_addr])
+	# print(mine_coins)
 	f = open('csv_files/duration.csv', 'w')
 	writer = csv.writer(f)
 	header = ["Number of Addresses in DB", "send duration", "listaddresses duration"]
@@ -13,19 +21,23 @@ def main(count):
 	address_types = ["legacy", "p2sh-segwit", "bech32"]
 	
 	for i in range(count):
+		if i % 10 == 0:
+			print(i, count)
 		addr = subprocess.check_output(["bitcoin-cli","-named","getnewaddress","address_type=" + address_types[i%3]])
-		args = ["bitcoin-cli","-named", "send","outputs=" + json.dumps({addr:0.00001}), "fee_rate=2"]
+		outputs = json.dumps({str(addr.decode("utf-8").replace("\n", "")):0.00001})
+		args = ["bitcoin-cli","-named", "send","outputs=" + outputs, "fee_rate=2"]
 
 		timeStarted = time.time()
-		subprocess.call(args)
+		call_output = subprocess.check_output(args)
 		timetaken = time.time() - timeStarted
 		
 		args = ["bitcoin-cli","listtransactions"]
 		timeStarted_list = time.time()
-		subprocess.call(args)
+		call_output = subprocess.call(args)
 		timetaken_list = time.time() - timeStarted_list
-		
-		writer.writerow([i,timetaken, timeStarted_list])
+
+		writer.writerow([i,timetaken, timetaken_list])
+		f.flush()
 
 	return
 	
