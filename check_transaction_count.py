77425adcd6deb3d1b725271f36f571a969b158f4
kirandevraj <thekirandevraj@gmail.com>
2021-07-26 22:59:28 +0530

    44k

--- check_transaction_count.py
+++ check_transaction_count.py
@@ -6,10 +6,7 @@ import csv
 
 def main():
 	args = ["bitcoin-cli","listaddressgroupings"]
-	timeStarted_list = time.time()
 	call_output = subprocess.check_output(args)
-	timeEnded_list = time.time()
-	timetaken_list = timeEnded_list - timeStarted_list
 	call_output = json.loads(call_output)
 	print(call_output)
 	print(len(call_output))
--- csv_files/plot_duration.py
+++ csv_files/plot_duration.py
@@ -8,15 +8,17 @@ with open('duration_test1.csv', newline='') as f:
     header = next(reader)
     rows = [[float(row[0]), float(row[1]), float(row[2])] for row in reader if row]
 
-plt.title("Time taken for send transaction")
-plt.plot(np.asarray(rows)[:,0], np.asarray(rows)[:,1])
-plt.xlabel("Number of addresses present in the wallet")
-plt.ylabel("Time taken for send transaction")
-plt.savefig("send.png")
-plt.clf()
+# plt.title("Time taken for send transaction")
+# plt.plot(np.arange(0, len(rows)), np.asarray(rows)[:,1])
+# plt.xlabel("Number of addresses present in the wallet")
+# plt.ylabel("Time taken for send transaction in seconds")
+# plt.savefig("send.png")
+# plt.clf()
 
-plt.title("Time taken for list transaction")
-plt.plot(np.asarray(rows)[:,0], np.asarray(rows)[:,2])
-plt.xlabel("Number of addresses present in the wallet")
-plt.ylabel("Time taken for listtransaction")
-plt.savefig("list.png")
+# plt.title("Time taken for list transaction")
+# plt.plot(np.arange(0, len(rows)), np.asarray(rows)[:,2])
+# plt.xlabel("Number of addresses present in the wallet")
+# plt.ylabel("Time taken for listtransaction")
+# plt.savefig("list.png")
+
+print(np.mean(np.asarray(rows)[:,1][-10000:]))
